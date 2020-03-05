6392dcae33792efca1dad6a0bbfd3b994c1d0056
Michelene Chon <michelenechon@gmail.com>
2020-03-05 09:34:03 -0800

    Add SignIn, SignUp, Copyright

--- jobswipe/src/App.js
+++ jobswipe/src/App.js
@@ -2,15 +2,14 @@ import React from 'react';
 import './App.css';
 import SignUp from './components/SignUp';
 import SignIn from './components/SignIn';
-import StickyFooter from './components/StickyFooter';
 
 function App() {
   return (
     <div className='App'>
       <main>
         <SignUp />
+        <hr />
         <SignIn />
-        <StickyFooter />
       </main>
     </div>
   );
---
+++ jobswipe/src/config.js
@@ -0,0 +1,4 @@
+export const APIURL =
+  window.location.hostname === 'localhost'
+    ? 'http://localhost:8000'
+    : 'https://jobble-backend.herokuapp.com';
