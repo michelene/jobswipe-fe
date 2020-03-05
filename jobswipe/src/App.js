c4e6ccb03c434583a6ece61fe171d23de08cb71e
Michelene Chon <michelenechon@gmail.com>
2020-03-05 09:48:36 -0800

    Add React Router and routes

--- jobswipe/src/App.js
+++ jobswipe/src/App.js
@@ -1,17 +1,22 @@
 import React from 'react';
+import { Route, Switch } from 'react-router-dom';
+
 import './App.css';
+import Home from './components/Home';
 import SignUp from './components/SignUp';
 import SignIn from './components/SignIn';
 
 function App() {
   return (
-    <div className='App'>
+    <>
       <main>
-        <SignUp />
-        <hr />
-        <SignIn />
+        <Switch>
+          <Route exact path='/' component={Home} />>
+          <Route exact path='/signup' component={SignUp} />>
+          <Route exact path='/signin' component={SignIn} />>
+        </Switch>
       </main>
-    </div>
+    </>
   );
 }
 
