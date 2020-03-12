7540821c56e1012bd170e3d04d4775ddfd4434c7
Michelene Chon <michelenechon@gmail.com>
2020-03-12 09:46:07 -0700

    Add AppBar

--- jobswipe/src/App.js
+++ jobswipe/src/App.js
@@ -4,6 +4,13 @@ import { Route, Switch } from 'react-router-dom';
 import './App.css';
 // import PublicHome from './components/PublicHome';
 // import PrivateHome from './components/PrivateHome';
+import AppBar from '@material-ui/core/AppBar';
+import Toolbar from '@material-ui/core/Toolbar';
+import MenuIcon from '@material-ui/icons/Menu';
+import Typography from '@material-ui/core/Typography';
+import Button from '@material-ui/core/Button';
+
+import IconButton from '@material-ui/core/IconButton';
 import SignUp from './components/SignUp';
 import SignIn from './components/SignIn';
 import ListView from './components/ListView';
@@ -14,6 +21,21 @@ function App() {
   return (
     <>
       <main>
+        <AppBar position='static'>
+          <Toolbar>
+            <IconButton
+              edge='start'
+              // className={classes.menuButton}
+              color='inherit'
+              aria-label='menu'
+            >
+              <MenuIcon />
+            </IconButton>
+            {/* <Typography variant='h6' className={classes.title}> */}
+            <Typography variant='h6'>jobswipe</Typography>
+            <Button color='inherit'>Login</Button>
+          </Toolbar>
+        </AppBar>
         <Switch>
           <Route exact path='/signup' component={SignUp} />>
           <Route exact path='/signin' component={SignIn} />>
